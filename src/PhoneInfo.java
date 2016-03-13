8038e2c5972ae0e0535b98f59e67f970d6ec91ca
fasdgoc <fasdgoc@gmail.com>
2016-03-14 02:59:17 +0900

    Add PhoneInfo.java for practice

---
+++ src/PhoneInfo.java
@@ -0,0 +1,28 @@
+
+public class PhoneInfo
+{
+  private String       name;
+  private String       phoneNumber;
+  private String       birthday;
+
+  public PhoneInfo(String name, String phoneNumber){
+    this.name = name;
+    this.phoneNumber = phoneNumber;
+    this.birthday = null;
+  }
+
+  public PhoneInfo(String name, String phoneNumber, String birthday){
+    this(name, phoneNumber);
+    this.birthday = birthday;
+  }
+
+  public void showPhoneInfo(){
+    System.out.println("name: "+ name);
+    System.out.println("phoneNumber: "+ phoneNumber);
+    
+    if(birthday != null)
+      System.out.println("birthday :"+ birthday);
+
+    System.out.println("");
+  }
+}
